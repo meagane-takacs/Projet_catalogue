f4379532cda2b0634110293127dce4f7f5df121d
meagane-takacs <meagane.takacs@le-campus-numerique.fr>
2020-02-04 15:18:08 +0100

    3-Etape 5 - Formulaire

--- catalogue4_formulaire.php
+++ catalogue4_formulaire.php
@@ -16,7 +16,7 @@ include ("functions.php");
     <meta charset="utf-8" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
           integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="style_catalogue3.css">
+    <link rel="stylesheet" href="style_catalogue4.css">
 </head>
 <body>
 
