971d9d005cb1f81a25c9c483f0c04d98d111e5be
meagane-takacs <meagane.takacs@le-campus-numerique.fr>
2020-02-06 11:21:20 +0100

    ajout_etape_5

---
+++ Etape4.php
@@ -0,0 +1,8 @@
+<?php
+$articles = [
+    ['photos/Bracelet1.jpg', 'Bracelet', 200],
+    ['photos/Montre.jpg','Montre',300],
+    ['photos/collier.jpg','Collier',300],
+
+
+    function afficheArticle ();
